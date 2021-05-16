44d3a7e080b6350bec0d10f8b8f929d9fb3cf8ac
filippoferrando <ferrandodfilippo@gmail.com>
2021-05-16 23:27:34 +0200

    telefono senza fili

--- Python/TelefonoSenzaFili/file.py
+++ Python/TelefonoSenzaFili/file.py
@@ -3,20 +3,22 @@ telefono senza fili in python
 '''
 import socket as sck
 
-HOST_CLIENT = ('x.x.x.x', 4096)
-HOST_SERVER = ('0.0.0.0', 4096)
+
+HOST_CLIENT = ('192.168.0.131', 7000)
+HOST_SERVER = ('192.168.0.133', 7000)
 
 def main():
-    client = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
-    server = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)    
     server.bind(HOST_SERVER)
+    client = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
+    server = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
 
-    data, addr = server.recvfrom(4096)
-    print(data.decode())
-    client.sendto(data.encode(), HOST_CLIENT)
-    
-    server.close()
-    client.close()
+    while True:
+        data, addr = server.recvfrom(4096)
+        print(data.decode())
+        client.sendto(data, HOST_CLIENT)
+        
+        server.close()
+        client.close()
 
 if __name__ == '__main__':
     main()
